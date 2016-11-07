eecb37d26d7a715dd14dab407d1792905cbc39d0
sirjudge <nico.a.judge@gmail.com>
2016-11-06 20:11:47 -0500

    changed findSet into getParentNode and getRootNode. also fixed some other things

--- EdgeWeightedGraph.java
+++ EdgeWeightedGraph.java
@@ -27,296 +27,330 @@ import java.util.Stack;
  ******************************************************************************/
 
 /**
- *  The {@code EdgeWeightedGraph} class represents an edge-weighted
- *  graph of vertices named 0 through <em>V</em> - 1, where each
- *  undirected edge is of type {@link Edge} and has a real-valued weight.
- *  It supports the following two primary operations: add an edge to the graph,
- *  iterate over all of the edges incident to a vertex. It also provides
- *  methods for returning the number of vertices <em>V</em> and the number
- *  of edges <em>E</em>. Parallel edges and self-loops are permitted.
- *  <p>
- *  This implementation uses an adjacency-lists representation, which 
- *  is a vertex-indexed array of @link{Bag} objects.
- *  All operations take constant time (in the worst case) except
- *  iterating over the edges incident to a given vertex, which takes
- *  time proportional to the number of such edges.
- *  <p>
- *  For additional documentation,
- *  see <a href="http://algs4.cs.princeton.edu/43mst">Section 4.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * The {@code EdgeWeightedGraph} class represents an edge-weighted graph of
+ * vertices named 0 through <em>V</em> - 1, where each undirected edge is of
+ * type {@link Edge} and has a real-valued weight. It supports the following two
+ * primary operations: add an edge to the graph, iterate over all of the edges
+ * incident to a vertex. It also provides methods for returning the number of
+ * vertices <em>V</em> and the number of edges <em>E</em>. Parallel edges and
+ * self-loops are permitted.
+ * <p>
+ * This implementation uses an adjacency-lists representation, which is a
+ * vertex-indexed array of @link{Bag} objects. All operations take constant time
+ * (in the worst case) except iterating over the edges incident to a given
+ * vertex, which takes time proportional to the number of such edges.
+ * <p>
+ * For additional documentation, see
+ * <a href="http://algs4.cs.princeton.edu/43mst">Section 4.3</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
 
 public class EdgeWeightedGraph {
-    private static final String NEWLINE = System.getProperty("line.separator");
+	private static final String NEWLINE = System.getProperty("line.separator");
 
-    private final int V;
-    private int E;
-    private Bag<Edge>[] adj;
-    private ArrayList<KrusNode> set;
-    private ArrayList<Edge> edgeySet;
-    private ArrayList<Edge> edgesLookedAt;
-    /**
-     * Initializes an empty edge-weighted graph with {@code V} vertices and 0 edges.
-     *
-     * @param  V the number of vertices
-     * @throws IllegalArgumentException if {@code V < 0}
-     */
-    public EdgeWeightedGraph(int V) {
-        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
-        this.V = V;
-        this.E = 0;
-        edgeySet = new ArrayList<>();
-        set = new ArrayList<>();
-        edgesLookedAt = new ArrayList<>();
-        adj = (Bag<Edge>[]) new Bag[V];
-        for (int v = 0; v < V; v++) {
-            adj[v] = new Bag<Edge>();
-        }
-    }
+	private final int V;
+	private int E;
+	private Bag<Edge>[] adj;
+	private ArrayList<KrusNode> set;
+	private ArrayList<Edge> edgeySet;
+	private ArrayList<Edge> edgesLookedAt;
 
-    
+	/**
+	 * Initializes an empty edge-weighted graph with {@code V} vertices and 0
+	 * edges.
+	 *
+	 * @param V
+	 *            the number of vertices
+	 * @throws IllegalArgumentException
+	 *             if {@code V < 0}
+	 */
+	public EdgeWeightedGraph(int V) {
+		if (V < 0)
+			throw new IllegalArgumentException("Number of vertices must be nonnegative");
+		this.V = V;
+		this.E = 0;
+		edgeySet = new ArrayList<>();
+		set = new ArrayList<>();
+		edgesLookedAt = new ArrayList<>();
+		adj = (Bag<Edge>[]) new Bag[V];
+		for (int v = 0; v < V; v++) {
+			adj[v] = new Bag<Edge>();
+		}
+	}
 
-    /**  
-     * Initializes an edge-weighted graph from an input stream.
-     * The format is the number of vertices <em>V</em>,
-     * followed by the number of edges <em>E</em>,
-     * followed by <em>E</em> pairs of vertices and edge weights,
-     * with each entry separated by whitespace.
-     *
-     * @param  in the input stream
-     * @throws IndexOutOfBoundsException if the endpoints of any edge are not in prescribed range
-     * @throws IllegalArgumentException if the number of vertices or edges is negative
-     */
-    public EdgeWeightedGraph(In in) {
-        this(in.readInt());
-        int E = in.readInt();
-        if (E < 0) throw new IllegalArgumentException("Number of edges must be nonnegative");
-        for (int i = 0; i < E; i++) {
-            int v = in.readInt();
-            int w = in.readInt();
-            double weight = in.readDouble();
-            Edge e = new Edge(v, w, weight);
-            addEdge(e);
-        }
-    }
+	/**
+	 * Initializes an edge-weighted graph from an input stream. The format is
+	 * the number of vertices <em>V</em>, followed by the number of edges
+	 * <em>E</em>, followed by <em>E</em> pairs of vertices and edge weights,
+	 * with each entry separated by whitespace.
+	 *
+	 * @param in
+	 *            the input stream
+	 * @throws IndexOutOfBoundsException
+	 *             if the endpoints of any edge are not in prescribed range
+	 * @throws IllegalArgumentException
+	 *             if the number of vertices or edges is negative
+	 */
+	public EdgeWeightedGraph(In in) {
+		this(in.readInt());
+		int E = in.readInt();
+		if (E < 0)
+			throw new IllegalArgumentException("Number of edges must be nonnegative");
+		for (int i = 0; i < E; i++) {
+			int v = in.readInt();
+			int w = in.readInt();
+			double weight = in.readDouble();
+			Edge e = new Edge(v, w, weight);
+			addEdge(e);
+		}
+	}
 
-    /**
-     * Initializes a new edge-weighted graph that is a deep copy of {@code G}.
-     *
-     * @param  G the edge-weighted graph to copy
-     */
-    public EdgeWeightedGraph(EdgeWeightedGraph G) {
-        this(G.V());
-        this.E = G.E();
-        for (int v = 0; v < G.V(); v++) {
-            // reverse so that adjacency list is in same order as original
-            Stack<Edge> reverse = new Stack<Edge>();
-            for (Edge e : G.adj[v]) {
-                reverse.push(e);
-            }
-            for (Edge e : reverse) {
-                adj[v].add(e);
-            }
-        }
-    }
+	/**
+	 * Initializes a new edge-weighted graph that is a deep copy of {@code G}.
+	 *
+	 * @param G
+	 *            the edge-weighted graph to copy
+	 */
+	public EdgeWeightedGraph(EdgeWeightedGraph G) {
+		this(G.V());
+		this.E = G.E();
+		for (int v = 0; v < G.V(); v++) {
+			// reverse so that adjacency list is in same order as original
+			Stack<Edge> reverse = new Stack<Edge>();
+			for (Edge e : G.adj[v]) {
+				reverse.push(e);
+			}
+			for (Edge e : reverse) {
+				adj[v].add(e);
+			}
+		}
+	}
 
+	/**
+	 * Returns the number of vertices in this edge-weighted graph.
+	 *
+	 * @return the number of vertices in this edge-weighted graph
+	 */
+	public int V() {
+		return V;
+	}
 
-    /**
-     * Returns the number of vertices in this edge-weighted graph.
-     *
-     * @return the number of vertices in this edge-weighted graph
-     */
-    public int V() {
-        return V;
-    }
+	/**
+	 * Returns the number of edges in this edge-weighted graph.
+	 *
+	 * @return the number of edges in this edge-weighted graph
+	 */
+	public int E() {
+		return E;
+	}
 
-    /**
-     * Returns the number of edges in this edge-weighted graph.
-     *
-     * @return the number of edges in this edge-weighted graph
-     */
-    public int E() {
-        return E;
-    }
+	// throw an IndexOutOfBoundsException unless {@code 0 <= v < V}
+	private void validateVertex(int v) {
+		if (v < 0 || v >= V)
+			throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (V - 1));
+	}
 
-    // throw an IndexOutOfBoundsException unless {@code 0 <= v < V}
-    private void validateVertex(int v) {
-        if (v < 0 || v >= V)
-            throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (V-1));
-    }
+	/**
+	 * Adds the undirected edge {@code e} to this edge-weighted graph.
+	 *
+	 * @param e
+	 *            the edge
+	 * @throws IndexOutOfBoundsException
+	 *             unless both endpoints are between {@code 0} and {@code V-1}
+	 */
+	public void addEdge(Edge e) {
+		int v = e.either();
+		int w = e.other(v);
+		validateVertex(v);
+		validateVertex(w);
+		adj[v].add(e);
+		adj[w].add(e);
+		E++;
+	}
 
-    /**
-     * Adds the undirected edge {@code e} to this edge-weighted graph.
-     *
-     * @param  e the edge
-     * @throws IndexOutOfBoundsException unless both endpoints are between {@code 0} and {@code V-1}
-     */
-    public void addEdge(Edge e) {
-        int v = e.either();
-        int w = e.other(v);
-        validateVertex(v);
-        validateVertex(w);
-        adj[v].add(e);
-        adj[w].add(e);
-        E++;
-    }
+	/**
+	 * Returns the edges incident on vertex {@code v}.
+	 *
+	 * @param v
+	 *            the vertex
+	 * @return the edges incident on vertex {@code v} as an Iterable
+	 * @throws IndexOutOfBoundsException
+	 *             unless {@code 0 <= v < V}
+	 */
+	public Iterable<Edge> adj(int v) {
+		validateVertex(v);
+		return adj[v];
+	}
 
-    /**
-     * Returns the edges incident on vertex {@code v}.
-     *
-     * @param  v the vertex
-     * @return the edges incident on vertex {@code v} as an Iterable
-     * @throws IndexOutOfBoundsException unless {@code 0 <= v < V}
-     */
-    public Iterable<Edge> adj(int v) {
-        validateVertex(v);
-        return adj[v];
-    }
+	/**
+	 * Returns the degree of vertex {@code v}.
+	 *
+	 * @param v
+	 *            the vertex
+	 * @return the degree of vertex {@code v}
+	 * @throws IndexOutOfBoundsException
+	 *             unless {@code 0 <= v < V}
+	 */
+	public int degree(int v) {
+		validateVertex(v);
+		return adj[v].size();
+	}
 
-    /**
-     * Returns the degree of vertex {@code v}.
-     *
-     * @param  v the vertex
-     * @return the degree of vertex {@code v}               
-     * @throws IndexOutOfBoundsException unless {@code 0 <= v < V}
-     */
-    public int degree(int v) {
-        validateVertex(v);
-        return adj[v].size();
-    }
+	/**
+	 * Returns all edges in this edge-weighted graph. To iterate over the edges
+	 * in this edge-weighted graph, use foreach notation:
+	 * {@code for (Edge e : G.edges())}.
+	 *
+	 * @return all edges in this edge-weighted graph, as an iterable
+	 */
+	public Iterable<Edge> edges() {
+		Bag<Edge> list = new Bag<Edge>();
+		for (int v = 0; v < V; v++) {
+			int selfLoops = 0;
+			for (Edge e : adj(v)) {
+				if (e.other(v) > v) {
+					list.add(e);
+				}
+				// only add one copy of each self loop (self loops will be
+				// consecutive)
+				else if (e.other(v) == v) {
+					if (selfLoops % 2 == 0)
+						list.add(e);
+					selfLoops++;
+				}
+			}
+		}
+		return list;
+	}
 
-    /**
-     * Returns all edges in this edge-weighted graph.
-     * To iterate over the edges in this edge-weighted graph, use foreach notation:
-     * {@code for (Edge e : G.edges())}.
-     *
-     * @return all edges in this edge-weighted graph, as an iterable
-     */
-    public Iterable<Edge> edges() {
-        Bag<Edge> list = new Bag<Edge>();
-        for (int v = 0; v < V; v++) {
-            int selfLoops = 0;
-            for (Edge e : adj(v)) {
-                if (e.other(v) > v) {
-                    list.add(e);
-                }
-                // only add one copy of each self loop (self loops will be consecutive)
-                else if (e.other(v) == v) {
-                    if (selfLoops % 2 == 0) list.add(e);
-                    selfLoops++;
-                }
-            }
-        }
-        return list;
-    }
+	/**
+	 * Returns a string representation of the edge-weighted graph. This method
+	 * takes time proportional to <em>E</em> + <em>V</em>.
+	 *
+	 * @return the number of vertices <em>V</em>, followed by the number of
+	 *         edges <em>E</em>, followed by the <em>V</em> adjacency lists of
+	 *         edges
+	 */
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append(V + " " + E + NEWLINE);
+		for (int v = 0; v < V; v++) {
+			s.append(v + ": ");
+			for (Edge e : adj[v]) {
+				s.append(e + "  ");
+			}
+			s.append(NEWLINE);
+		}
+		return s.toString();
+	}
 
-    /**
-     * Returns a string representation of the edge-weighted graph.
-     * This method takes time proportional to <em>E</em> + <em>V</em>.
-     *
-     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
-     *         followed by the <em>V</em> adjacency lists of edges
-     */
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        s.append(V + " " + E + NEWLINE);
-        for (int v = 0; v < V; v++) {
-            s.append(v + ": ");
-            for (Edge e : adj[v]) {
-                s.append(e + "  ");
-            }
-            s.append(NEWLINE);
-        }
-        return s.toString();
-    }
+	public void makeSet(int x) {
+		KrusNode kNode = new KrusNode();
+		set.add(kNode);
+	}
 
-    public void makeSet(int x){
-        KrusNode kNode = new KrusNode();
-        set.add(kNode);
-    }
+	public KrusNode getParentNode(int x) {
+		KrusNode kNode = set.get(x); // get the node from the set
+		if (kNode.getParentNode().equals(kNode))
+			return kNode; // if the parent node is the node (it is the root
+							// node) return that node
+		else
+			return kNode.getParentNode();
+	}
 
-    //private Bag<Edge>[] adj;
-    public KrusNode findSet(int x){
-    	//System.out.println(x);
-    	//printSet();
-    	//printEdgeySet();
-        KrusNode kNode = set.get(x);
-        if(kNode.getParentNode().equals(kNode)) return kNode;
-        return set.get(x).getParentNode();
-    }
+	public KrusNode getParentNode(KrusNode krusIn) {
+		if (krusIn.getParentNode().equals(krusIn))
+			return krusIn; // if the parent node is the node (it is the root
+							// node) return that node
+		else
+			return krusIn.getParentNode();
+	}
 
-    public void union(KrusNode x,KrusNode y){
-        if(x.getSize() > y.getSize()){
-            y.setParentNode(x.getParentNode());
-            y.plusOneSize();
-        }
-        else{
-            x.setParentNode(y.getParentNode());
-            x.plusOneSize();
-        }
-    }
+	public KrusNode getRootNode(KrusNode kNode){
+		if(kNode.getParentNode().equals(kNode)) return kNode;
+		else return getRootNode(kNode.getParentNode());
+	}
 
-    public void printSet(){
-    	System.out.print("[");
-    	for(KrusNode n : set){
-    		System.out.print(n + ",");
-    	}
-    	System.out.println("]");
-    }
-    public void printEdgeySet(){
-    	System.out.print("[");
-    	for(Edge e: edgeySet){
-    		System.out.print(e+ ",");
-    	}
-    	System.out.println("]");
-    }
-    
-    public void kruskal(){
-        for(Bag<Edge> b: adj){
-            for(Edge e: b){
-                edgeySet.add(e);
-            }
-        }
-        Collections.sort(edgeySet);//sort the list of edges implimenting krusNode
-        
-        for(Edge e: edgeySet){ //go through list of edges, initialize the set arrayList
-        	int v = e.getV();  //get each vertex
-            int w = e.getW();
-            makeSet(v);
-            makeSet(w);
-        }
-        
-        for(Edge e : edgeySet){ //loop through again, find set on each vertex
-        	int v = e.getV();
-            int w = e.getW();
-        	KrusNode setV = findSet(v);
-            KrusNode setW = findSet(w);
-            if(!setV.equals(setW)){ //if they have the same root node, it's a cycle, don't print
-            	union(setV, setW);
-            	if(!edgesLookedAt.contains(e)){
-            		edgesLookedAt.add(e);
-            		System.out.println(e);
-            	}
-            }
-        }   
-    }
+	
+	
+	
+	public void union(KrusNode k1, KrusNode k2) {
+		if (k1.getSize() > k2.getSize()) {
+			k2.setParentNode(k1.getParentNode());
+			k2.plusOneSize();
+		} else {
+			k1.setParentNode(k2.getParentNode());
+			k1.plusOneSize();
+		}
+	}
 
+	public void printSet() {
+		System.out.print("[");
+		for (KrusNode n : set) {
+			System.out.print(n + ",");
+		}
+		System.out.println("]");
+	}
 
+	public void printEdgeySet() {
+		System.out.print("[");
+		for (Edge e : edgeySet) {
+			System.out.print(e + ",");
+		}
+		System.out.println("]");
+	}
 
-    /**
-     * Unit tests the {@code EdgeWeightedGraph} data type.
-     *
-     * @param args the command-line arguments
-     */
-    
-    public static void main(String[] args) {
-        In in = new In(args[0]);
-        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
-        StdOut.println(G);
-        G.kruskal();
-    }
+	public void kruskal() {
+		for (Bag<Edge> b : adj) {
+			for (Edge e : b) {
+				edgeySet.add(e);
+			}
+		}
+		Collections.sort(edgeySet);// sort the bag of edges
+
+		for (Edge e : edgeySet) { // go through list of edges, initialize the set of verticies
+			int v = e.getV(); // get each vertex
+			int w = e.getW();
+			makeSet(v);
+			makeSet(w);
+		}
+
+		for (Edge e : edgeySet) { // loop through again, calls findSet on each vertex
+			int v = e.getV(); // get each vertex
+			int w = e.getW();
+			
+			// find the root node for each vertex
+			KrusNode pNodeV = getRootNode(getParentNode(v));
+			KrusNode pNodeW = getRootNode(getParentNode(w));
+			
+			// if they have the same root node, it's a cycle, don't print
+			//System.out.println("I am checking: " + e);
+			if (pNodeV != pNodeW) {
+				// if we have not already looked at the edge in the set
+				if (!edgesLookedAt.contains(e)) {
+					edgesLookedAt.add(e); // add it to the edgesLookedAt
+					System.out.println(e); // print out the edge
+					union(pNodeV, pNodeW);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Unit tests the {@code EdgeWeightedGraph} data type.
+	 *
+	 * @param args
+	 *            the command-line arguments
+	 */
+
+	public static void main(String[] args) {
+		In in = new In(args[0]);
+		EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+		StdOut.println(G);
+		G.kruskal();
+	}
 
 }
